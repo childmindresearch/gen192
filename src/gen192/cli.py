701b29eda1c4733bd3fb9762c544f921e7d2fe01
Florian Rupprecht <floruppr@gmail.com>
2023-12-18 17:03:49 +0100

    Minor refactoring

--- src/gen192/cli.py
+++ src/gen192/cli.py
@@ -1,16 +1,14 @@
-import base64
 import copy
-import hashlib
 import json
 import pathlib as pl
 import shutil
 from dataclasses import dataclass
-from typing import Any, Dict, Generator, Iterable, List, Optional, Sequence
+from typing import Any, Dict, Generator, List
 
 import yaml
 
 from .cpac_config_extractor import check_cpac_config, fetch_and_expand_cpac_configs
-from .utils import filesafe, print_warning
+from .utils import aslist, b64_urlsafe_hash, filesafe, multi_del, multi_get, multi_set, print_warning
 
 PIPELINE_NAMES = {
     "ABCD": "abcd-options",
@@ -72,9 +70,6 @@ PIPELINE_STEPS: List[PipelineStep] = [
 """Pipeline steps and their paths in the C-PAC config to include in the generation."""
 
 
-# Utilities
-
-
 @dataclass
 class PipelineConfig:
     """Represents a C-PAC pipeline configuration"""
@@ -99,83 +94,6 @@ class PipelineConfig:
         if self.notes is not None:
             with open(self.file.with_suffix(".notes.txt"), "w") as handle:
                 handle.write(self.notes)
-
-
-def multi_get(obj: dict, index: Iterable) -> Optional[Any]:  # noqa: ANN401
-    """
-    Gets a value from a nested dictionary.
-    Returns None if the path does not exist.
-    """
-    for i in index:
-        if not isinstance(obj, dict) or i not in obj:
-            return None
-        obj = obj[i]
-    return obj
-
-
-def multi_set(obj: dict, index: Sequence, value: Any) -> bool:  # noqa: ANN401
-    """
-    Sets a value in a nested dictionary.
-    Returns True if the path exists or was able to be created
-    and the value was set.
-    """
-    for idx, i in enumerate(index):
-        if not isinstance(obj, dict):
-            return False
-
-        if idx == len(index) - 1:
-            obj[i] = value
-            return True
-
-        if i not in obj:
-            obj[i] = {}
-
-        obj = obj[i]
-    assert False
-
-
-def multi_del(obj: dict, index: Sequence) -> Optional[Any]:  # noqa: ANN401
-    """
-    Deletes a value from a nested dictionary.
-    Returns the value if the path exists and
-    the value was deleted.
-    """
-    for idx, i in enumerate(index):
-        if not isinstance(obj, dict):
-            return None
-
-        if idx == len(index) - 1:
-            if i in obj:
-                val = obj[i]
-                del obj[i]
-                return val
-            return None
-
-        if i not in obj:
-            return None
-
-        obj = obj[i]
-    assert False
-
-
-def aslist(obj: Any) -> list:  # noqa: ANN401
-    """
-    Converts an object to a list. If the object is
-    already a list, it is returned as is.
-    """
-    if isinstance(obj, list):
-        return obj
-    return [obj]
-
-
-def b64_urlsafe_hash(s: str) -> str:
-    """
-    Hashes a string and returns a base64 urlsafe encoded version of the hash.
-    """
-    return base64.urlsafe_b64encode(hashlib.sha1(s.encode()).digest()).decode().replace("=", "")
-
-
-# Generation
 
 
 @dataclass
@@ -205,9 +123,6 @@ class PipelineCombination:
 def iter_pipeline_combis() -> Generator[PipelineCombination, Any, None]:
     """
     Iterate over all possible parameter combinations.
-
-    From the heights of these pyramids, forty centuries look down on us.
-    - Napoleon Bonaparte
     """
     for pipeline_id in PIPELINE_NAMES.keys():
         for pipeline_perturb_id in PIPELINE_NAMES.keys():
--- src/gen192/utils.py
+++ src/gen192/utils.py
@@ -1,7 +1,9 @@
+import base64
+import hashlib
 import os
 import re
 from contextlib import contextmanager
-from typing import Generator
+from typing import Any, Generator, Sequence
 
 
 @contextmanager
@@ -27,3 +29,77 @@ def filesafe(s: str, replacement: str = "-") -> str:
 def print_warning(msg: str) -> None:
     """Prints a colored warning message to the console"""
     print(f"\033[93mWARNING: {msg}\033[0m")
+
+
+def multi_get(obj: dict, index: Sequence) -> Any | None:  # noqa: ANN401
+    """
+    Gets a value from a nested dictionary.
+    Returns None if the path does not exist.
+    """
+    for i in index:
+        if not isinstance(obj, dict) or i not in obj:
+            return None
+        obj = obj[i]
+    return obj
+
+
+def multi_set(obj: dict, index: Sequence, value: Any) -> bool:  # noqa: ANN401
+    """
+    Sets a value in a nested dictionary.
+    Returns True if the path exists or was able to be created
+    and the value was set.
+    """
+    for idx, i in enumerate(index):
+        if not isinstance(obj, dict):
+            return False
+
+        if idx == len(index) - 1:
+            obj[i] = value
+            return True
+
+        if i not in obj:
+            obj[i] = {}
+
+        obj = obj[i]
+    assert False
+
+
+def multi_del(obj: dict, index: Sequence) -> Any | None:  # noqa: ANN401
+    """
+    Deletes a value from a nested dictionary.
+    Returns the value if the path exists and
+    the value was deleted.
+    """
+    for idx, i in enumerate(index):
+        if not isinstance(obj, dict):
+            return None
+
+        if idx == len(index) - 1:
+            if i in obj:
+                val = obj[i]
+                del obj[i]
+                return val
+            return None
+
+        if i not in obj:
+            return None
+
+        obj = obj[i]
+    assert False
+
+
+def aslist(obj: Any) -> list:  # noqa: ANN401
+    """
+    Converts an object to a list. If the object is
+    already a list, it is returned as is.
+    """
+    if isinstance(obj, list):
+        return obj
+    return [obj]
+
+
+def b64_urlsafe_hash(s: str) -> str:
+    """
+    Hashes a string and returns a base64 urlsafe encoded version of the hash.
+    """
+    return base64.urlsafe_b64encode(hashlib.sha1(s.encode()).digest()).decode().replace("=", "")
